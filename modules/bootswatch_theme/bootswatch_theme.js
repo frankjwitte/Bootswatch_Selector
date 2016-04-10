0408d05dad83278d239122053f2be477463d3ebd
Frank Witte <frank@franksworld.nl>
2016-04-10 22:09:16 +0200

    First commit

---
+++ modules/bootswatch_theme/bootswatch_theme.js
@@ -0,0 +1,59 @@
+/**
+ * Created by frank on 10-4-16.
+ */
+(function () {
+    "use strict";
+
+    angular.module("bootswatch.theme", [])
+        .directive("bootswatchTheme", function BowerTheme($http) {
+
+            var urlv2 = "https://bootswatch.com/api/2.json";
+            var urlv3 = "https://bootswatch.com/api/3.json";
+
+            return {
+                bindToController: true,
+                scope: {
+                    "defaultTheme": "@"
+                },
+                controller: function () {
+                    var vm = this;
+                    vm.themes = [];
+                    vm.selectedTheme = {};
+
+                    vm.setTheme = function () {
+                        var link = document.querySelector("#bootswatch_theme");
+                        if (link == null) {
+                            link = document.createElement("link");
+                            link.id = "bootswatch_theme";
+                            link.rel = "stylesheet";
+                            document.head.appendChild(link);
+                        }
+                        link.href = vm.selectedTheme.cssCdn;
+                        localStorage.setItem("BootswatchTheme", vm.selectedTheme.name);
+                    };
+
+                    function init() {
+                        $http.get(urlv3)
+                            .success(function (data) {
+                                vm.themes = data.themes;
+                                var defaultTheme = localStorage.getItem("BootswatchTheme") || vm.defaultTheme || vm.themes[0].name;
+                                vm.selectedTheme = vm.themes.find(function (theme) {
+                                    return theme.name === defaultTheme;
+                                });
+                                vm.setTheme();
+                            });
+                    }
+
+                    init();
+                },
+                controllerAs: "vm",
+                template: "<select class='form-control' " +
+                "                   ng-options='theme as theme.name for theme in vm.themes'" +
+                "                   ng-model='vm.selectedTheme' " +
+                "                   ng-change='vm.setTheme()'>" +
+                "</select>"
+            };
+        })
+    ;
+
+}());
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Bootswatch Selector
+## Intro
+Bootstrap is a great way to easily setup a decent looking web application.
+For developers it is a great time saver. To make it even more interesting there are many
+themes around for Bootstrap to give the web application a custom look. 
+[Bootswatch](http://bootswatch.com Bootswatch) presents a dozen free themes.
+You can apply them by replacing the Bootstrap CSS with one of the themes. There is a nice API
+that lists up the available themes and references to a CDN version.
+There is themes available for both V2 and V3 of Bootstrap.
+
+I want to make a theme selector directive for AngularJS where you can just drop in a tag like:
+
+    <bootswatch-theme version="3" default-theme="spacelab">
+    </bootswatch-theme>
+
